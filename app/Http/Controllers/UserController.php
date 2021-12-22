e81f2bbbe8e45d5a5da5a242a34865436f708c5e
zemix30004 <eugenezm@gmail.com>
2021-12-22 14:33:19 +0200

    added requests permission roles controllers

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::latest()->paginate(10);
+
+        return view('users.index', compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(User $user, StoreUserRequest $request)
+    {
+        //For demo purposes only. When creating user or inviting a user
+        // you should create a generated random password and email it to the user
+        $user->create(array_merge($request->validated(), [
+            'password' => 'test'
+        ]));
+
+        return redirect()->route('users.index')
+            ->withSuccess(__('User created successfully.'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        return view('users.show', [
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        return view('users.edit', [
+            'user' => $user,
+            'userRole' => $user->roles->pluck('name')->toArray(),
+            'roles' => Role::latest()->get()
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(User $user, UpdateUserRequest $request)
+    {
+        $user->update($request->validated());
+
+        $user->syncRoles($request->get('role'));
+
+        return redirect()->route('users.index')
+            ->withSuccess(__('User updated successfully.'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return redirect()->route('users.index')
+            ->withSuccess(__('User deleted successfully.'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\PermissionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,26 +28,51 @@ Route::group(['namespace' => 'App\Http\Controllers'], function () {
     /**
      * Home Routes
      */
-    Route::get('/', 'HomeController@index')->name('home.index');
+    Route::get('/', [HomeController::class, 'index'])->name('home.index');
 
     Route::group(['middleware' => ['guest']], function () {
         /**
          * Register Routes
          */
-        Route::get('/register', 'RegisterController@show')->name('register.show');
-        Route::post('/register', 'RegisterController@register')->name('register.perform');
+        Route::get('/register', [RegisterController::class, 'show'])->name('register.show');
+        Route::post('/register', [RegisterController::class, 'register'])->name('register.perform');
 
         /**
          * Login Routes
          */
-        Route::get('/login', 'LoginController@show')->name('login.show');
-        Route::post('/login', 'LoginController@login')->name('login.perform');
+        Route::get('/login', [LoginController::class, 'show'])->name('login.show');
+        Route::post('/login', [LoginController::class, 'login'])->name('login.perform');
     });
 
     Route::group(['middleware' => ['auth']], function () {
         /**
          * Logout Routes
          */
-        Route::get('/logout', 'LogoutController@perform')->name('logout.perform');
+        Route::get('/logout', [LogoutController::class, 'perform'])->name('logout.perform');
+
+
+        Route::group(['prefix' => 'user'], function () {
+            Route::get('/', [UserController::class, 'index'])->name('users.index');
+            Route::get('/create', [UserController::class, 'create'])->name('users.create');
+            Route::post('/create', [UserController::class, 'store'])->name('users.store');
+            Route::get('/{user}/show', [UserController::class, 'show'])->name('users.show');
+            Route::get('/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+            Route::patch('/{user}/update', [UserController::class, 'update'])->name('users.update');
+            Route::delete('/{user}/delete', [UserController::class, 'destroy'])->name('users.destroy');
+        });
+        /**
+         * User Routes
+         */
+        Route::group(['prefix' => 'post'], function () {
+            Route::get('/', [PostController::class, 'index'])->name('posts.index');
+            Route::get('/create', [PostController::class, 'create'])->name('posts.create');
+            Route::post('/create', [PostController::class, 'store'])->name('posts.store');
+            Route::get('/{post}/show', [PostController::class, 'show'])->name('posts.show');
+            Route::get('/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+            Route::patch('/{post}/update', [PostController::class, 'update'])->name('posts.update');
+            Route::delete('/{post}/delete', [PostController::class, 'destroy'])->name('posts.destroy');
+        });
+        Route::resource('roles', RoleController::class);
+        Route::resource('permissions', PermissionController::class);
     });
 });
